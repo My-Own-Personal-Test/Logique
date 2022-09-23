6199f355fcadc3baac39621d7c2c215c1fc92e96
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-09-23 22:39:24 +0700

    feat(home screen): add home screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+const input = ref("");
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container flex items-center justify-end flex-col pb-[26px]">
+    <input
+      type="text"
+      class="w-[280px] rounded-full text-center"
+      placeholder="Artist / Album / Title"
+      v-model="input"
+      style="height: 40px; outline: none"
+    />
+    <button
+      class="mt-[15px] w-[280px] rounded-full text-white"
+      style="background-color: rgba(255, 255, 255, 0.2); height: 40px"
+    >
+      Search
+    </button>
+  </div>
 </template>
+
+<style>
+.container {
+  background-image: url("../assets/vector/logo.svg");
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
